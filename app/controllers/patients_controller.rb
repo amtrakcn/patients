a92ee67f8dca0d5169746172124a0d52b95cd01e
Charles Shen <amtrakcn@gmail.com>
2014-06-16 02:33:47 -0700

    whitelisted params

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,4 +8,24 @@ class PatientsController < ApplicationController
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+    @patients = Patient.where(deleted: false)
+    render :index
+  end
+
+  private
+    def patient_params
+      params.require(:patient).permit(:first_name, :middle_name, :last_name, :date_of_birth, :gender, :status, :location_id, :view_count)
+    end
+    
 end
